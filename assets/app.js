dd55bdc9f8b4bdd1cbfc52aa361e248e0a5238b0
Rachael Gladwin <rachaelgladwin@Rachaels-iMac.local>
2020-03-16 17:57:34 -0600

    added initial function

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+    $("#forminfo").on("submit", function (e) {
+        e.preventDefault();
+
+        var artist = $("input[name='artist']").val();
+        console.log(artist);
+
+        var queryURL = "http://newsapi.org/v2/everything?q=" + artist +
+            "&" +
+            "apiKey=c338cad50e1e4c6d8aa41548e7259844";
+        console.log(queryURL);
+
+        $.get(queryURL, function (data) {
+            console.log(data);
+            $("#artistHeadline").append(data.title);
+            $("#newsDisplay").append(data.content)
+        });
+    });
+});

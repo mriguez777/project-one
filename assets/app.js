610080e11fbf0ec49e8c0d254c6039abfd0a128d
Rachael Gladwin <rachaelgladwin@Rachaels-iMac.local>
2020-03-16 19:30:09 -0600

    got one api to call on another api

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     $("#forminfo").on("submit", function (e) {
         e.preventDefault();
@@ -22,10 +21,38 @@ $(document).ready(function () {
             $("#newsDisplay").append(data.articles[0].description);
 
         }).then(function () {
-//insert next api call
+            //insert next api call
+            $("gifDisplay").empty();
+
+            var queryURL2 =
+                "https://api.giphy.com/v1/gifs/search?q=" +
+                artist +
+                "&api_key=o2BMu32QNHXQZs2A5dgV6kntwDSDTrXU&limit=10";
+            console.log(queryURL2);
+
+            $.get(queryURL2, function (response2) {
+                console.log(response2);
+                var results2 = response2.data;
+                for (var i = 0; i < results2.length; i++) {
+                    // Creating and storing a div tag
+                    var newgifDiv = $("<div>");
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + results2[i].rating);
+                    // Creating and storing an image tag
+                    var gifImage = $("<img>");
+                    // Setting the src attribute of the image to a property pulled off the result item
+                    gifImage.attr("src", results2[i].images.fixed_height.url);
+                    // Appending the paragraph and image tag to the animalDiv
+                    newgifDiv.append(p);
+                    newgifDiv.prepend(gifImage);
+                    // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+                    $("#gifImage").prepend(newgifDiv);
+
+                };
+
+            });
 
         });
 
-    });
-
-});
+    })
+})

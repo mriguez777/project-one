778ec2790be62941528635f2a9c72686884f963d
Rachael Gladwin <rachaelgladwin@Rachaels-iMac.local>
2020-03-21 21:33:18 -0600

    tested it so many times google said we've reached our daily limit

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,48 @@
 $(document).ready(function () {
+    /**
+   * Sample JavaScript code for youtube.search.list
+   * See instructions for running APIs Explorer code samples locally:
+   * https://developers.google.com/explorer-help/guides/code_samples#javascript
+   */
+
+    // function authenticate() {
+    //     return gapi.auth2.getAuthInstance()
+    //         .signIn({ scope: "https://www.googleapis.com/auth/youtube.force-ssl" })
+    //         .then(function () { console.log("Sign-in successful"); },
+    //             function (err) { console.error("Error signing in", err); });
+    // }
+    // function loadClient() {
+    //     gapi.client.setApiKey("AIzaSyBA6-_OD-6JJj0-Gr4FjtF6KIeZeBQxXQ8");
+    //     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+    //         .then(function () { console.log("GAPI client loaded for API"); },
+    //             function (err) { console.error("Error loading GAPI client for API", err); });
+    // }
+    // // Make sure the client is loaded and sign-in is complete before calling this method.
+    // function execute() {
+    //     return gapi.client.youtube.search.list({
+    //         "part": "snippet",
+    //         "maxResults": 3,
+    //         "q": artist
+    //     })
+    //         .then(function (response) {
+    //             // Handle the results here (response.result has the parsed body).
+    //             console.log("Response", response);
+    //         },
+    //             function (err) { console.error("Execute error", err); });
+    // }
+    // gapi.load("client:auth2", function () {
+    //     gapi.auth2.init({ client_id: "YOUR_CLIENT_ID" });
+    // });
+
+    // $("#ytAuth").on("click", function () {
+    //     authenticate().then(loadClient);
+    //     console.log("we authorized something");
+    // });
+
+    // $("#ytExe").on("click", function () {
+    //     execute();
+    //     console.log("we executed something");
+    // });
 
     $("#forminfo").on("submit", function (e) {
         e.preventDefault();
@@ -101,7 +145,7 @@ $(document).ready(function () {
 
 
         }).then(function () {
-
+            search(artist);
             var queryURL2 = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + artist + "&api-key=N1PAfjpWLPTkYo3aG4S8CFaFm9UlS6mv"
 
             console.log(queryURL2);
@@ -134,59 +178,83 @@ $(document).ready(function () {
 
         }).then(function () {
 
-            function authenticate() {
-                return gapi.auth2.getAuthInstance()
-                    .signIn({ scope: "https://www.googleapis.com/auth/youtube.force-ssl" })
-                    .then(function () { console.log("Sign-in successful"); },
-                        function (err) { console.error("Error signing in", err); });
+
+
+            var tag = document.createElement('script');
+            tag.id = 'iframe-demo';
+            tag.src = 'https://www.youtube.com/iframe_api';
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+            var player;
+
+            function onYouTubeIframeAPIReady() {
+                player = new YT.Player('existing-iframe-example', {
+                    events: {
+                        'onReady': onPlayerReady,
+                        'onStateChange': onPlayerStateChange
+                    }
+                });
+
             }
-            function loadClient() {
-                gapi.client.setApiKey("AIzaSyBA6-_OD-6JJj0-Gr4FjtF6KIeZeBQxXQ8");
-                return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-                    .then(function () { console.log("GAPI client loaded for API"); },
-                        function (err) { console.error("Error loading GAPI client for API", err); });
+            function onPlayerReady(event) {
+                document.getElementById('existing-iframe-example').style.borderColor = '#FF6D00';
+                event.target.setVolume(100);
+                event.target.playVideo();
             }
-            // Make sure the client is loaded and sign-in is complete before calling this method.
-            function execute() {
-                return gapi.client.youtube.search.list({
-                    "part": "snippet",
-                    "maxResults": 3,
-                    "q": artist
-                })
-                    .then(function (response5) {
-                        // Handle the results here (response.result has the parsed body).
-                        console.log("Response", response5.result);
-
-                        youTubeSearch
-                    },
-                        function (err) { console.error("Execute error", err); });
+            function changeBorderColor(playerStatus) {
+                var color;
+                if (playerStatus == -1) {
+                    color = "#37474F"; // unstarted = gray
+                } else if (playerStatus == 0) {
+                    color = "#FFFF00"; // ended = yellow
+                } else if (playerStatus == 1) {
+                    color = "#33691E"; // playing = green
+                } else if (playerStatus == 2) {
+                    color = "#DD2C00"; // paused = red
+                } else if (playerStatus == 3) {
+                    color = "#AA00FF"; // buffering = purple
+                } else if (playerStatus == 5) {
+                    color = "#FF6DOO"; // video cued = orange
+                }
+                if (color) {
+                    document.getElementById('existing-iframe-example').style.borderColor = color;
+                }
             }
-            gapi.load("client:auth2", function () {
-                gapi.auth2.init({ client_id: "YOUR_CLIENT_ID" });
-            });
+            function onPlayerStateChange(event) {
+                changeBorderColor(event.data);
 
-            execute();
-            authenticate().then(loadClient);
-
-
-            // var settings = {
-            //     "async": true,
-            //     "crossDomain": true,
-            //     "url": "https://coolguruji-youtube-to-mp3-download-v1.p.rapidapi.com/?id=lF-jPBnZ098",
-            //     "method": "GET",
-            //     "headers": {
-            //         "x-rapidapi-host": "coolguruji-youtube-to-mp3-download-v1.p.rapidapi.com",
-            //         "x-rapidapi-key": "286743b9e8mshca96ef72bc84158p10a340jsn84bc27c45ef1",
-            //     }
-            // }
-
-            // $.ajax(settings).done(function (response5) {
-            //     console.log(response5);
-            // });
-
-
-        })
+            }
+        }).catch(function (err) { console.error("Rachael signing in", err) });
 
     });
+
+    function search(artistIn) {
+        
+        console.log(artistIn);
+
+        var apikey = "AIzaSyBA6-_OD-6JJj0-Gr4FjtF6KIeZeBQxXQ8"
+        $.ajax({
+            method: "GET",
+            urlG: "https//www.googleapis.com/youtube/vs/search",
+            data: {
+                key: apikey,
+                q: artistIn,
+                part: "snippet",
+                maxResults: 3,
+                type: "video",
+                videoEmbeddable: true,
+            }
+        }).done((data) => {
+
+            var videos = data.items;
+            console.log(videos)
+
+        }).catch(function(err){console.log("you have an:" + JSON.stringify(err))});
+    }
+
+    console.log("we made it!");
+
+
 });
 
